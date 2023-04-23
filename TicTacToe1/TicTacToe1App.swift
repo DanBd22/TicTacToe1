17657381294a2bbfac28fac44ffea0878ab26387
DanBd22 <dan.badarau04@gmail.com>
2023-04-23 14:32:35 +0300

    Initial Commit

---
+++ TicTacToe1/TicTacToe1App.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe1App.swift
+//  TicTacToe1
+//
+//  Created by Badarau Dan on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
